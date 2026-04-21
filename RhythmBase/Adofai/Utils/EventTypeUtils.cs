e9a236c151eb80442dd26bb36b759e20a16d41ad
OLDREDSTONE <OLDRedstone@users.noreply.github.com>
2026-04-21 11:36:19 +0800

    解决变基冲突

--- RhythmBase/Adofai/Utils/EventTypeUtils.cs
+++ RhythmBase/Adofai/Utils/EventTypeUtils.cs
@@ -1,19 +1,18 @@
-namespace RhythmBase.Adofai.Utils
+namespace RhythmBase.Adofai.Utils;
+
+/// <summary>
+/// Provides utility methods for working with event types.
+/// </summary>
+/// <remarks>This class contains static methods that assist in handling, analyzing, or processing event types.
+/// It is designed to be used as a helper class and cannot be instantiated.</remarks>
+public static partial class EventTypeUtils
 {
-	/// <summary>
-	/// Provides utility methods for working with event types.
-	/// </summary>
-	/// <remarks>This class contains static methods that assist in handling, analyzing, or processing event types.
-	/// It is designed to be used as a helper class and cannot be instantiated.</remarks>
-	public static partial class EventTypeUtils
-	{
-    }
-	/// <summary>
-	/// Provides utility methods for working with filter types.
-	/// </summary>
-	/// <remarks>This class contains static methods that assist in handling, analyzing, or processing filter types.
-	/// It is designed to be used as a helper class and cannot be instantiated.</remarks>
-	public static partial class FilterTypeUtils
-	{
-	}
+}
+/// <summary>
+/// Provides utility methods for working with filter types.
+/// </summary>
+/// <remarks>This class contains static methods that assist in handling, analyzing, or processing filter types.
+/// It is designed to be used as a helper class and cannot be instantiated.</remarks>
+public static partial class FilterTypeUtils
+{
 }
