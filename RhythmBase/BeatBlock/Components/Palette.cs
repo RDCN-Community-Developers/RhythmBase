2470656b06d6e869b0bf8e1b50b433315394ad63
OLDREDSTONE <OLDRedstone@users.noreply.github.com>
2026-04-29 13:53:46 +0800

    添加调色板

---
+++ RhythmBase/BeatBlock/Components/Palette.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RhythmBase.BeatBlock.Components
+{
+    public struct Palette()
+    {
+        private readonly RDColor[] colors = new RDColor[8];
+        public RDColor this[int index]
+        {
+            get => colors[index];
+            set => colors[index] = value;
+        }
+        public static Palette Default => new()
+        {
+            [0] = White,
+            [1] = Black,
+            [2] = Red,
+            [3] = Blue,
+            [4] = Green,
+            [5] = Yellow,
+            [6] = Pink,
+            [7] = Cyan,
+        };
+        public static RDColor Black => 0xFF000000;
+        public static RDColor Red => 0xFFFF0000;
+        public static RDColor Blue => 0xFF0000FF;
+        public static RDColor Grey => 0xFFD4D4D4;
+        public static RDColor Gray => 0xFFD4D4D4;
+        public static RDColor White => 0xFFFFFFFF;
+        public static RDColor Pink => 0xFFFF00FF;
+        public static RDColor Magenta => 0xFFFF00FF;
+        public static RDColor Green => 0xFF00FF00;
+        public static RDColor Yellow => 0xFFFFFF00;
+        public static RDColor Cyan => 0xFF00FFFF;
+    }
+}
