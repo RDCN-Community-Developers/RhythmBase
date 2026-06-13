2769791ee617e6204aec2369c8abf8ddc3b5b610
OLDREDSTONE <OLDRedstone@users.noreply.github.com>
2026-06-13 14:32:49 +0800

    适配短 PlayerTypeGroup

--- RhythmBase.RhythmDoctor/RhythmDoctor/Converters/PlayerTypeGroupConverter.cs
+++ RhythmBase.RhythmDoctor/RhythmDoctor/Converters/PlayerTypeGroupConverter.cs
@@ -1,3 +1,4 @@
+using RhythmBase.Global.Converters;
 using RhythmBase.RhythmDoctor.Components;
 using System.Text.Json;
 using System.Text.Json.Serialization;
@@ -5,32 +6,36 @@ using System.Text.Json.Serialization;
 namespace RhythmBase.RhythmDoctor.Converters;
 
 [JsonConverterFor(typeof(PlayerTypeGroup))]
-internal class PlayerTypeGroupConverter : JsonConverter<PlayerTypeGroup>
+internal class PlayerTypeGroupConverter : MetadataJsonConverter<PlayerTypeGroup>
 {
-	public override PlayerTypeGroup Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+	public override PlayerTypeGroup Read(ref Utf8JsonReader reader, Type typeToConvert, MetadataJsonSerializerOptions options)
 	{
 		if (reader.TokenType != JsonTokenType.StartArray)
 			throw new JsonException($"Expected StartObject token, but got {reader.TokenType}.");
-		reader.Read();
 		PlayerTypeGroup group = new();
-		for (int i = 0; i < 16; i++)
-		{
-			if (reader.TokenType == JsonTokenType.String)
+		if (options.Strictness == JsonStrictness.Strict)
+			for (int i = 0; i < 16; i++)
 			{
+				reader.Read();
+				JsonException.ThrowIfNotMatch(ref reader, JsonTokenType.String);
 				group[i] = RhythmBase.Global.Converters.EnumConverter.TryParse(reader.ValueSpan, out PlayerType type) ? type : PlayerType.NoChange;
 			}
-			else
+		else
+		{
+			int i = 0;
+			while (reader.Read() && reader.TokenType != JsonTokenType.EndArray)
 			{
-				throw new JsonException($"Expected String token, but got {reader.TokenType}.");
+				JsonException.ThrowIfNotMatch(ref reader, JsonTokenType.String);
+				group[i] = RhythmBase.Global.Converters.EnumConverter.TryParse(reader.ValueSpan, out PlayerType type) ? type : PlayerType.NoChange;
+				i++;
 			}
-			reader.Read();
 		}
 		return reader.TokenType != JsonTokenType.EndArray
 			? throw new JsonException($"Expected EndArray token, but got {reader.TokenType}.")
 			: group;
 	}
 
-	public override void Write(Utf8JsonWriter writer, PlayerTypeGroup value, JsonSerializerOptions options)
+	public override void Write(Utf8JsonWriter writer, PlayerTypeGroup value, MetadataJsonSerializerOptions options)
 	{
 		writer.WriteStartArray();
 		for (int i = 0; i < 16; i++)
